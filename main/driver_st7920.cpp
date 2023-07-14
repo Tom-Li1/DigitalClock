d936e86b7a37fabd6c7db564bb074502e9cfd819
Ethan Li <liyikang1024@163.com>
2023-07-15 01:53:00 +1000

    print characters

--- main/driver_st7920.cpp
+++ main/driver_st7920.cpp
@@ -4,7 +4,7 @@
 
 
 volatile static char instructionSet_ST7920 = 'B'; // Current instruction set: 'B' - basic, 'E' - extended.
-volatile static bool displayStatus_ST7920 = true; // true - display ON, false - display OFF. (irrelevant to backlight)
+volatile static bool displayStatus_ST7920 = true; // true - display ON, false - display OFF. (irrelevant to backlight).
 volatile static bool underlineCursorStatus_ST7920 = true; // true - show underline cursor, false - hide underline cursor.
 volatile static bool blinkCursorStatus_ST7920 = true; // true - show blink cursor, false - hide blink cursor.
 
@@ -43,6 +43,7 @@ static void chooseInstructionSet_ST7920(char choice) {
    direction = 'L': cursor or display moves left
    direction = 'R': cursor or display moves right */
 static void setEntryMode_ST7920(char mode, char direction) {
+  chooseInstructionSet_ST7920('B');
   if (mode == 'C' and direction == 'R') sendInstruction_ST7920(0b0000000110);
   else if (mode == 'C' and direction == 'L') sendInstruction_ST7920(0b0000000100);
   else if (mode == 'D' and direction == 'R') sendInstruction_ST7920(0b0000000101);
@@ -77,7 +78,20 @@ static void homeCursor_ST7920() {
 }
 
 
-/* Initialize the display according to the instructions in the datasheet. */
+/* Correctly set the cursor position according to cursor change pattern.
+   32 full sized 16*16 characters can be presented by 128*64 display at the same time.
+   The position is a number between 0 (top left corner) and 31 (bottom right corner). */
+static void setCursor_ST7920(byte position) {
+  if ((position >= 0 && position <= 7) || (position >= 24 && position <= 31)) {
+    sendInstruction_ST7920(0b0010000000 | position);
+  } else if (position >= 8 && position <= 15) {
+    sendInstruction_ST7920(0b0010000000 | position + 8);
+  } else if (position >= 16 && position <= 23) {
+    sendInstruction_ST7920(0b0010000000 | position - 8);
+  }
+}
+
+
 void initialize_ST7920() {
   // Activate and config SPI communication.
   SPI.begin();
@@ -105,8 +119,6 @@ void initialize_ST7920() {
 }
 
 
-/* Fill DDRAM with space (clear half height and full sized characters).
-   Move cursor to home (left end of the first line) by setting address counter to 0x00. */
 void clearCharacterDisplay_ST7920() {
   chooseInstructionSet_ST7920('B');
   sendInstruction_ST7920(0b0000000001);
@@ -114,11 +126,9 @@ void clearCharacterDisplay_ST7920() {
 }
 
 
-/* option: 'D' - display, 'C' - cursor, 'B' - cursor blink.
-   status: true - ON, false - OFF. */ 
 void setDisplayStatus_ST7920(char option, bool status) {
   short instruction = 0b0000001000;
-  chooseInstructionSet_ST7920('B');
+
   switch (option) {
     case 'D':
       displayStatus_ST7920 = status;
@@ -130,101 +140,93 @@ void setDisplayStatus_ST7920(char option, bool status) {
       blinkCursorStatus_ST7920 = status;
       break;
   }
+
   if (displayStatus_ST7920) instruction |=  0b0000000100;
   if (underlineCursorStatus_ST7920) instruction |= 0b0000000010;
   if (blinkCursorStatus_ST7920) instruction |= 0b0000000001;
+
+  chooseInstructionSet_ST7920('B');
   sendInstruction_ST7920(instruction);
 }
 
 
-void printHalfCharacters();
+void printHalfCharacters_ST7920(char chars[], byte length, byte row, byte column) {
+  byte position = row * 16 + column; // range of position for half chars is 0-63
+  setCursor_ST7920(position / 2); // move cursor to specified position
+
+  if (position % 2 == 1) sendInstruction_ST7920(0b1000100000); // add a space at the beginning when needed
+  for (byte i = 0; i < length; i++) {
+    if (position == 16 || position == 32 || position == 48)
+      setCursor_ST7920(position / 2); // move to the next line by set cursor on appropriate position on the display module
+    sendInstruction_ST7920(0b1000000000 | chars[i]);
+    position += 1;
+  }
+  if (position % 2 == 1) sendInstruction_ST7920(0b1000100000); // add a space at the end when needed
+}
 
 
-void printFullCharacters();
+void printFullCharacters_ST7920(short chars[], byte length, byte row, byte column) {
+  byte position = row * 8 + column;
+  setCursor_ST7920(position);
+
+  for (byte i = 0; i < length; i++) {
+    if (position == 8 || position == 16 || position == 24)
+      setCursor_ST7920(position);
+    sendInstruction_ST7920(0b1000000000 | chars[i] >> 8 & 0b0000000011111111);
+    sendInstruction_ST7920(0b1000000000 | chars[i] & 0b0000000011111111);
+    position += 1;
+  }
+}
 
 
 void test() {
   initialize_ST7920();
 
-  sendInstruction_ST7920(0b1010100100);
-  sendInstruction_ST7920(0b1011001010);
-  delay(1000);
+  delay(500);
+  short chineseChars[] = {
+    0xCEAA, 0xC1CB, 0xB6A9, 0xB5A5, 0xBCB8, 0xBAF5, 0xC5E3, 0xCBAF,
+    0xB5E3, 0xCDB7, 0xB9FE, 0xD1FC, 0xBECD, 0xB2EE, 0xCFC2, 0xB9F2,
+    0xC6A8, 0xB4F3, 0xB5E3, 0xCAC2, 0xB2BB, 0xB8D2, 0xB5C3, 0xD7EF,
+    0xD2BB, 0xC4EA, 0xB5BD, 0xCDB7, 0xB2BB, 0xC0EB, 0xB8DA, 0xCEBB};
 
-  sendInstruction_ST7920(0b1010100100);
-  sendInstruction_ST7920(0b1011001110);
-  delay(1000);
-
-  setEntryMode_ST7920('C', 'L');
-
-  sendInstruction_ST7920(0b1010100100);
-  sendInstruction_ST7920(0b1011001111);
-  delay(1000);
-
-  sendInstruction_ST7920(0b1010100100);
-  sendInstruction_ST7920(0b1010001111);
-  delay(1000);
-
-  setEntryMode_ST7920('C', 'R');
-
-  sendInstruction_ST7920(0b1010111010);
-  sendInstruction_ST7920(0b1010111010);
-  delay(1000);
-
-  sendInstruction_ST7920(0b1011010111);
-  sendInstruction_ST7920(0b1011010110);
-  delay(1000);
-
-  sendInstruction_ST7920(0b1011001111);
-  sendInstruction_ST7920(0b1011010100);
-  delay(1000);
-
-  sendInstruction_ST7920(0b1011001010);
-  sendInstruction_ST7920(0b1010111110);
-  delay(1000);
-
-  homeCursor_ST7920();
-  delay(1000);
-
-  setDisplayStatus_ST7920('C', false);
-  setDisplayStatus_ST7920('B', false);
-
-  // problematic for loop
-  for (int i = 0; i < 8; i++) {
-    shiftDisplay_ST7920('R');
-    delay(500);
-  }
-  for (int i = 0; i < 8; i++) {
-    shiftDisplay_ST7920('L');
-    delay(500);
-  }
-
+  printFullCharacters_ST7920(chineseChars, 32, 0, 0);
   
-  for (int i = 0; i < 8; i++) {
-    moveCursor_ST7920('R');
-    delay(500);
+  /*
+  for (int i = 0; i < 1; i++) {
+    printHalfCharacters_ST7920("Hello world!", 12, 0, 0);
+    delay(1000);
+    printHalfCharacters_ST7920("Hola, mundo!", 12, 1, 1);
+    delay(1000);
+    printHalfCharacters_ST7920("Bojo le mod!", 12, 2, 2);
+    delay(1000);
+    printHalfCharacters_ST7920("Kon'nichiwa!", 12, 3, 3);
+    delay(1000);
+
+    setDisplayStatus_ST7920('D', false);
+    delay(200);
+    setDisplayStatus_ST7920('D', true);
+    delay(200);
+    setDisplayStatus_ST7920('D', false);
+    delay(200);
+    setDisplayStatus_ST7920('D', true);
+    delay(200);
+    setDisplayStatus_ST7920('D', false);
+    delay(200);
+
+    clearCharacterDisplay_ST7920();
+    setDisplayStatus_ST7920('D', true);
   }
-  for (int i = 0; i < 8; i++) {
-    moveCursor_ST7920('L');
-    delay(500);
-  }
-  
-  setDisplayStatus_ST7920('D', false);
-  delay(1000);
-  setDisplayStatus_ST7920('D', true);
-  delay(1000);
 
-  setDisplayStatus_ST7920('B', false);
-  delay(1000);
-  setDisplayStatus_ST7920('B', true);
-  delay(1000);
-
-  setDisplayStatus_ST7920('C', false);
-  delay(1000);
-  setDisplayStatus_ST7920('C', true);
-  delay(1000);
-
+  while (true) {
+  printHalfCharacters_ST7920("This is a display test for 128*64 LCD display module with ST7920", 64, 0, 0);
+  delay(3000);
   clearCharacterDisplay_ST7920();
-  delay(1000);
+  printHalfCharacters_ST7920("If you can see these texts, then the program and module are fine", 64, 0, 0);
+  delay(3000);
+  clearCharacterDisplay_ST7920();
+  }
+  */
+  
 }
 
 /* DDRAM address is used to locate the character going to be shown.
--- main/driver_st7920.h
+++ main/driver_st7920.h
@@ -1,6 +1,7 @@
 #ifndef DRIVER_ST7920_H
 #define DRIVER_ST7920_H
 
+
 /* Wire Connection for Arduino Nano:
    Display Module - Nano Board
    VCC - 5V
@@ -13,9 +14,11 @@
    BLA (backlight LED +) - 5V (some other module needs 3.3v)
    BLK (backlight LED -) - GND */
 
+
 #include <Arduino.h>
 
-// 8*16 half height non-ascii character code
+
+// 8*16 half height non-ascii character (icon) code
 #define SMILE_REVERSE 0x01
 #define SMILE 0x02
 #define HEART 0x03
@@ -49,6 +52,34 @@
 #define TRIANGLE_DOWN 0x1f
 #define TRIANGLE_UP_HOLLOW 0x7f
 
+
 void test();
+
+// Initialize the display according to the instructions in the datasheet.
+void initialize_ST7920(); 
+
+// Clear all characters and home cursor (reset DDRAM data and address).
+void clearCharacterDisplay_ST7920(); 
+
+/* Turn on/off character and cursor display (not backlight)
+   option: 'D' - display, 'C' - cursor, 'B' - cursor blink.
+   status: true - ON, false - OFF. */
+void setDisplayStatus_ST7920(char option, bool status);
+
+/* Print a series of half height 8*16 characters.
+   The order is from left to right and from top to bottom.
+   chars: An array of chars or half height character code for icons.
+   length: number of half characters in array.
+   row (0-3): The row number (vertical position) of the first character.
+   column (0-15): The column number (horizontal position) of the first character. */
+void printHalfCharacters(char chars[], char length, char row, char column);
+
+/* Print a series of 16*16 Chinese/Japanese/Korean characters.
+   The order is from left to right and from top to bottom.
+   chars: An array of GB codes of your characters.
+   length: number of character codes in the array.
+   row (0-3): The row number (vertical position) of the first character.
+   column (0-7): The column number (horizontal position) of the first character. */
+void printFullCharacters_ST7920(short chars[], byte length, byte row, byte column);
 
 #endif
